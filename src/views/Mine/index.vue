18a64793cb9cb2ef93e176800d68ac453feab3de
andmi <m18401504987@163.com>
2019-08-14 15:15:09 +0800

    add createComponents

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Header title="喵喵登陆"></Header>
+       <Login></Login>
+    </div>
+</template>
+<script>
+import Header from '@/components/Header'
+import Login from '@/components/Login'
+export default {
+  components:{
+    Login,
+    Header
+  }
+}
+</script>
+<style lang="scss">
+    
+</style>
