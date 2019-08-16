5bda967356212da03e0130e1c7bbfce3dc862dc7
andmi <m18401504987@163.com>
2019-08-16 11:10:28 +0800

    add setData

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+// vue.config.js
+module.exports = {
+    // 修改的配置
+    // 将baseUrl: '/api',改为baseUrl: '/',
+    devServer: {
+        proxy: {
+            '/api': {
+                target:"http://39.97.33.178/api",
+                changeOrigin: true,
+                ws: true,
+                pathRewrite: {
+                  '^/api': ''
+                }
+            }
+        }
+    }
+    
+  }
+  // .env.development
+  VUE_APP_BASE_API='/api'
